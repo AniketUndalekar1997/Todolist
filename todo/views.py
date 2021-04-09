f889853644f74c0011289de0026246efc920d98e
Aniket Undalekar <aniketundalekar@gmail.com>
2021-04-09 20:09:43 +0530

    third commit

---
+++ todo/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+from django.contrib import messages
+
+
+# Create your views here.
+def index(request):
+    todo = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title=request.POST['title']
+        )
+        if new_todo is None:
+            messages.error(request, 'Enter task')
+        else:
+            new_todo.save()
+            return redirect('/')
+
+    return render(request, 'index.html', {'todos': todo})
+
+
+def del_task(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
+
+
+
